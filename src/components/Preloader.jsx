f3f576761c9a9c72f34f723684ab7d242c42878e
Adarsh Sreekumar <adarshsreekumar298@gmail.com>
2025-11-24 12:58:05 +0530

    add components&pages

---
+++ src/components/Preloader.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Preloader() {
+  return (
+    <div className='h-screen flex justify-center items-center'>
+        <img src="https://i.pinimg.com/originals/94/ce/ee/94ceee9bbd60682a601af2f61f120afd.gif" alt="" />
+    </div>
+  )
+}
+
+export default Preloader
